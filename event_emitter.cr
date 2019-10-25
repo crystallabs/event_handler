7bd907c26e70afc994ce85a65f7d6cfe7a22e1b4
Davor Ocelic <docelic@crystallabs.io>
2019-10-25 04:00:16 +0200

    Improve documentation.

--- event_emitter.cr
+++ event_emitter.cr
@@ -1,6 +1,18 @@
 module Crysterm
+  # Implementation of Crysterm's event system, and basically the functional core of Crysterm.
+  # `EventEmitter` is used by `Node`, the class at the lowest level.
   module EventEmitter
 
+    # Record for easy and convenient creation of handler procs.
+    #
+    # If *run_once* is true, handler will be automatically removed after it is invoked once.
+    #
+    # ```
+    # handler = ClickedEvent::Handler.new { |e| p "Coordinates are x=#{e.x} y=#{e.y}"; true }
+    #
+    # c = SomeClass.new
+    # c.on ClickedEvent, handler
+    # ```
     record Handler(T), handler : T, run_once : Bool = false do
       delegate :call, to: @handler
     end
@@ -18,37 +30,47 @@ module Crysterm
          emit NewListenerEvent, type, ->(ev : Event){ handler.call(ev.as({{e.id}})) }
         end
 
+        # Installs *handler* as a handler for event of type *type*.
         def on(type : {{e.id}}.class, handler : Proc({{e.id}}, Bool))
           internal_add type, handler, false
         end
-        def on(type : {{e.id}}.class, &block : {{e.id}} -> Bool)
-          on type, block
+        # ditto
+        def on(type : {{e.id}}.class, &handler : {{e.id}} -> Bool)
+          on type, handler
         end
 
+        # Installs *handler* as a handler for event of type *type*.
+        # It triggers it at most once, after which the handler is automatically removed.
         def once(type : {{e.id}}.class, handler : Proc({{e.id}}, Bool))
           internal_add type, handler, true
         end
-        def once(type : {{e.id}}.class, &block : {{e.id}} -> Bool)
-          once type, block
+        # ditto
+        def once(type : {{e.id}}.class, &handler : {{e.id}} -> Bool)
+          once type, handler
         end
-        def once(*arg) add_once(*arg) end
 
+        # Uninstalls *handler* as a handler for event of type *type*.
         def off(type : {{e.id}}.class, handler : Proc({{e.id}}, Bool))
           if _event_{{event_name}}.delete Handler.new handler
            emit RemoveListenerEvent, type, ->(ev : Event){ handler.call(ev.as({{e.id}})) }
           end
         end
 
-        def removeAllListeners
+        # Removes all handlers for *type*.
+        # The removal is immediate, and no `RemoveListenerEvent` events are emitted.
+        def removeAllListeners(type : {{e.id}}.class)
           # Blessed does it this way by just emptying the handlers, without emitting any RemoveListenerEvents
           _event_{{event_name}}.clear
         end
 
+        # Returns array of currently installed handlers for *type*.
         def listeners(type : {{e.id}}.class)
           _event_{{event_name}}
         end
 
-        def _emit(type : {{class_name}}.class, obj : {{class_name}})
+        # Low-level function used to execute handlers and almost nothing else.
+        # Regular users should use `#emit` instead.
+        private def _emit(type : {{class_name}}.class, obj : {{class_name}})
           if _event_{{event_name}}.empty?
             if type == ErrorEvent
               raise Exception.new obj.to_s
@@ -63,10 +85,12 @@ module Crysterm
 
           ret != false
         end
-        def _emit(type : {{class_name}}.class, *args)
+        # ditto
+        private def _emit(type : {{class_name}}.class, *args)
           emit type, {{e.id}}.new *args
         end
 
+        # Emits event *type*, along with supplied parameters.
         def emit(type : {{e.id}}.class, *args)
          #emit EventEvent, type, ->(ev : Event){ handler.call(ev.as({{e.id}})) }
 
