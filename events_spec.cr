13b9a864f08f5e9be6aba8b51fc15b4a2d85f3d6
Davor Ocelic <docelic@crystallabs.io>
2019-10-27 13:48:59 +0100

    Add missing tests

--- events_spec.cr
+++ events_spec.cr
@@ -6,56 +6,91 @@ require "../src/event_emitter"
 
 module Crysterm
 
-	class TestEvents
-		include ::Crysterm::EventEmitter
-	end
+  class TestEvents
+    include ::Crysterm::EventEmitter
+  end
 
-	describe Crysterm do
-		it "works" do
-			Event.should be_truthy
-		end
+  describe Crysterm do
+    it "works" do
+      Event.should be_truthy
+    end
 
-		it "works for builtin events" do
-			count = 0
-			c = TestEvents.new
-			c.on(NewListenerEvent){|e| count += 1; true}
-			count.should eq 1
+    it "works for builtin events" do
+      count = 0
+      c = TestEvents.new
+      c.on(NewListenerEvent){|e| count += 1; true}
+      count.should eq 1
 
-			h1 = ->(e : NewListenerEvent) { count += 1; true}
-			c.on NewListenerEvent, h1
-			count.should eq 3
+      c.listeners(NewListenerEvent).size.should eq 1
 
-			h2 = NewListenerEvent::Handler.new { count += 1; true}
-			c.on NewListenerEvent, h2
-			count.should eq 6
+      h1 = ->(e : NewListenerEvent) { count += 1; true}
+      c.on NewListenerEvent, h1
+      count.should eq 3
 
-			c.on(RemoveListenerEvent){|e| count -= 1; true}
-			count.should eq 9
+      c.listeners(NewListenerEvent).size.should eq 2
 
-			c.off(NewListenerEvent, h1)
-			count.should eq 8
+      h2 = NewListenerEvent::Handler.new { count += 1; true}
+      c.on NewListenerEvent, h2
+      count.should eq 6
 
-			c.off(NewListenerEvent, h2)
-			count.should eq 7
+      c.listeners(NewListenerEvent).size.should eq 3
 
-			# Already removed, so shouldn't change anything
-			c.off(NewListenerEvent, h2)
-			count.should eq 7
-		end
+      c.on(RemoveListenerEvent){|e| count -= 1; true}
+      count.should eq 9
 
-		it "works for custom events" do
-			count = 0
-			c = TestEvents.new
-			c.on(ClickedEvent){|e| count += 1; true}
-			count.should eq 0
+      c.listeners(RemoveListenerEvent).size.should eq 1
 
-			c.emit ClickedEvent, 1,1
-			count.should eq 1
+      c.off(NewListenerEvent, h1)
+      count.should eq 8
 
-			c.emit ClickedEvent, 1,1
-			c.emit ClickedEvent, 1,1
-			count.should eq 3
-		end
+      c.listeners(NewListenerEvent).size.should eq 2
 
-	end
+      c.off(NewListenerEvent, h2)
+      count.should eq 7
+
+      c.listeners(NewListenerEvent).size.should eq 1
+
+      # Already removed, so shouldn't change anything
+      c.off(NewListenerEvent, h2)
+      count.should eq 7
+
+      c.removeAllListeners(NewListenerEvent)
+      c.listeners(NewListenerEvent).should be_empty
+    end
+
+    it "works for custom events" do
+      count = 0
+      c = TestEvents.new
+      c.on(ClickedEvent){|e| count += 1; true}
+      count.should eq 0
+
+      c.emit ClickedEvent, 1,1
+      count.should eq 1
+
+      c.emit ClickedEvent, 1,1
+      c.emit ClickedEvent, 1,1
+      count.should eq 3
+
+      c.removeAllListeners(ClickedEvent)
+      c.listeners(ClickedEvent).should be_empty
+    end
+
+    it "raises if no handlers for ErrorEvent" do
+      count = 0
+      c = TestEvents.new
+
+      c.on(::Crysterm::ErrorEvent){|e| count += 1; true}
+      count.should eq 0
+
+      c.emit(::Crysterm::ErrorEvent, "Big error message")
+      count.should eq 1
+
+      c.removeAllListeners(::Crysterm::ErrorEvent)
+
+      expect_raises Exception do
+        c.emit(::Crysterm::ErrorEvent, "Big error message")
+      end
+    end
+
+  end
 end
