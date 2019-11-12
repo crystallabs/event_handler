22aca098357e4d29932f544013bf350b300294ef
Davor Ocelic <docelic@crystallabs.io>
2019-11-12 18:16:10 +0100

    Add @@at_beginning, @@at_end

--- src/events.cr
+++ src/events.cr
@@ -1,4 +1,7 @@
 module EventHandler
+  # These events are defined using `class_record` instead of the `event`
+  # macro due to: https://github.com/crystal-lang/crystal/issues/8463
+
   # Event used for emitting exceptions.
   # If an exception is emitted using this event and there are no handlers
   # subscribed to it, the exception will instead be raised.
@@ -13,15 +16,14 @@ module EventHandler
     event_type : ::EventHandler::Event.class,
     event : ::EventHandler::Event
 
-  # Meta event, emitted whenever a handler is added for any event,
-  # including itself.
+  # Meta event, emitted whenever a handler is added for any event, including itself.
   #
-  # When AddHandlerEvent and RemoveHandlerEvent are emitted, they invoke
-  # their handlers with the Wrapper object. A wrapper object for each
-  # handler is implicitly created on every `on`, and in addition to providing
+  # When `AddHandlerEvent` and `RemoveHandlerEvent` are emitted, they invoke
+  # their handlers with the `Wrapper` object. A wrapper object for each
+  # handler is implicitly created on every `on()`, and in addition to providing
   # access to the handlers themselves, wrappers also contain the values of
-  # arguments used during handler subscription (values of once?, async?,
-  # and at). This allows listeners on these meta events full insight into
+  # arguments used during handler subscription (values of *once?*, *async?*,
+  # and *at*). This allows listeners on these meta events full insight into
   # the added handlers and their settings.
   class_record AddHandlerEvent < Event,
     event : ::EventHandler::Event.class,
@@ -29,12 +31,12 @@ module EventHandler
 
   # Meta event, emitted whenever a handler is removed from any event, including itself.
   #
-  # When AddHandlerEvent and RemoveHandlerEvent are emitted, they invoke
-  # their handlers with the Wrapper object. A wrapper object for each
-  # handler is implicitly created on every `on`, and in addition to providing
+  # When `AddHandlerEvent` and `RemoveHandlerEvent` are emitted, they invoke
+  # their handlers with the `Wrapper` object. A wrapper object for each
+  # handler is implicitly created on every `on()`, and in addition to providing
   # access to the handlers themselves, wrappers also contain the values of
-  # arguments used during handler subscription (values of once?, async?,
-  # and at). This allows listeners on these meta events full insight into
+  # arguments used during handler subscription (values of *once?*, *async?*,
+  # and *at*). This allows listeners on these meta events full insight into
   # the added handlers and their settings.
   class_record RemoveHandlerEvent < Event,
     event : ::EventHandler::Event.class,
