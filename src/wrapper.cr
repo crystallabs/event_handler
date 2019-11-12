02da44a20ff0d8bf34c6ba0219c0cc03d02d8283
Davor Ocelic <docelic@crystallabs.io>
2019-11-12 18:17:01 +0100

    Improve Wrapper documentation

--- src/wrapper.cr
+++ src/wrapper.cr
@@ -1,14 +1,15 @@
 module EventHandler
   # Generic class wrapping each installed handler.
   #
-  # It is generally not used directly; but is exposed by the system in response to `on()`
-  # and is sent as an argument to `AnyEvent`, `AddHandlerEvent`, and `RemoveHandlerEvent`.
+  # This class is generally not used directly, but is implicitly created and returned in response to `on()`.
   #
-  # It provides additional information about the handler, such as values for
-  # *once*, *async*, and initial position/index in the list of event's handlers.
+  # It is also sent as an argument to emitted `AddHandlerEvent` and `RemoveHandlerEvent` events.
   #
-  # The information about the index is informational. The best use is to check it for
-  # values of 0 or -1, indicating that handler was added at the beginning or end of
+  # It holds settings associated with each installed handler; its values for
+  # *once*, *async*, and *at* (the initial insertion position in the list of handlers).
+  #
+  # *at* value is informational. Probably best use is to check for
+  # values of 0 or -1, indicating the handler's insertion at the beginning or end of
   # list respectively.
   class Wrapper(T)
     getter  handler : T
@@ -16,9 +17,15 @@ module EventHandler
     getter? once : Bool
     getter? async : Bool
     getter  at : Int32
+
     def initialize(@handler : T, @once, @async, @at, hash = nil)
       @handler_hash = hash || @handler.hash
     end
+
+    #def initialize(once, async, at, hash = nil, &handler : T)
+    #  initialize handler, once, async, at, hash
+    #end
+
     def call(obj, async)
       async = @async if async.nil?
       if async
